bc3dd134b917595de1da2c10cd92783bffac163f
Andre Manchanthasouk <dremanchan@gmail.com>
2022-02-23 15:40:34 -0600

    created models for pet and petowner.

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -7,5 +7,19 @@ namespace pet_hotel
 {
     public enum PetBreedType {}
     public enum PetColorType {}
-    public class Pet {}
+    public class Pet {
+    
+        public int id {get; set;}
+
+        [Required]
+        public string name {get; set;}
+        [Required]
+        public PetBreedType breed {get; set;}
+        public DateTime checkedInAt {get; set;}
+
+        [Required, ForeignKey("PetOwners")]
+        public int petOwnerid {get; set;}
+
+        
+    }
 }
