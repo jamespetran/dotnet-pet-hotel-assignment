4d4688bc34ecf795cb00babf8d823312a49f2bf0
James Petran <jm.ptrn@gmail.com>
2022-02-23 16:32:40 -0600

    created new migration to recreate the database

---
+++ Migrations/20220223223128_UpdatePetTableForNullTries.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_bakery.Migrations
+{
+    public partial class UpdatePetTableForNullTries : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "checkedInAt",
+                table: "Pets",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_petOwnerid",
+                table: "Pets",
+                column: "petOwnerid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_PetOwners_petOwnerid",
+                table: "Pets",
+                column: "petOwnerid",
+                principalTable: "PetOwners",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_PetOwners_petOwnerid",
+                table: "Pets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pets_petOwnerid",
+                table: "Pets");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "checkedInAt",
+                table: "Pets",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+        }
+    }
+}
