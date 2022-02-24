e3bfc367ee72384926cc704ff8357d239c5bdf1c
James Petran <jm.ptrn@gmail.com>
2022-02-24 13:48:58 -0600

    json converter to int is working

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -1,49 +1,50 @@
 using System.Collections.Generic;
 using System;
-// using Newtonsoft.Json.Converters;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
 
 
 namespace pet_hotel
 {
-    // [JsonConverter(typeof(StringEnumConverter))]
-
-    public enum PetBreedType {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum PetBreedType
+    {
         [EnumMember(Value = "breed")]
         Shepherd, Poodle, Beagle, Bulldog, Terrier, Boxer, Labrador, Retriever
     }
 
-    [JsonConverter(typeof(StringEnumConverter))]
-    public enum PetColorType {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum PetColorType
+    {
         [EnumMember(Value = "color")]
         White, Black, Golden, Tricolor, Spotted
     }
-    public class Pet {
-    
-        public int id {get; set;}
+    public class Pet
+    {
+
+        public int id { get; set; }
 
         [Required]
-        public string name {get; set;}
-        
+        public string name { get; set; }
+
         [Required]
-        // [JsonProperty("breed")]
-        // [JsonConverter(typeof(JsonStringEnumConverter))]
-        public PetBreedType breed {get; set;}
-        
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetBreedType breed { get; set; }
+
         [Required]
-        // [JsonProperty("color")]
-        // [JsonConverter(typeof(StringEnumConverter))]
-        public PetColorType color {get; set;}
-        
-        public DateTime? checkedInAt {get; set;}
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetColorType color { get; set; }
+
+        public DateTime? checkedInAt { get; set; }
 
         [Required, ForeignKey("PetOwners")]
-        public int petOwnerid {get; set;}
+        public int petOwnerid { get; set; }
 
-        public PetOwner petOwner {get; set;}
+        public PetOwner petOwner { get; set; }
 
-        
+
     }
 }
