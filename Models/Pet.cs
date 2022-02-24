45fdecc9f8ce80505663a1d39fcc87a4a2cbf869
James Petran <jm.ptrn@gmail.com>
2022-02-24 13:35:44 -0600

    trying to figure out json

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -1,14 +1,23 @@
-using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System;
+// using Newtonsoft.Json.Converters;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System;
+using System.Runtime.Serialization;
+
 
 namespace pet_hotel
 {
+    // [JsonConverter(typeof(StringEnumConverter))]
+
     public enum PetBreedType {
+        [EnumMember(Value = "breed")]
         Shepherd, Poodle, Beagle, Bulldog, Terrier, Boxer, Labrador, Retriever
     }
+
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum PetColorType {
+        [EnumMember(Value = "color")]
         White, Black, Golden, Tricolor, Spotted
     }
     public class Pet {
@@ -17,9 +26,15 @@ namespace pet_hotel
 
         [Required]
         public string name {get; set;}
+        
         [Required]
+        // [JsonProperty("breed")]
+        // [JsonConverter(typeof(JsonStringEnumConverter))]
         public PetBreedType breed {get; set;}
-
+        
+        [Required]
+        // [JsonProperty("color")]
+        // [JsonConverter(typeof(StringEnumConverter))]
         public PetColorType color {get; set;}
         
         public DateTime? checkedInAt {get; set;}
